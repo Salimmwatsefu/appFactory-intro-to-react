d9e019e13d4b2e08d00bfeb13318339d36391b76
Salim Mwatsefu Mwarika <sjmwatsefu@gmail.com>
2024-07-17 19:10:37 +0300

    Add about section

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <>
-      <div className=' bg-gray-200'>
+      <div className=' bg-gray-100'>
 
        <Navbar />
 
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function About() {
+
+  const skills = [
+  'HTML', 'JAVASCRIPT', 'PYTHON', 'C++', 'RUBY', 'DJANGO', 'FLASK', 'REACT', 'ANGULAR'
+
+]
   return (
-    <div>About</div>
+    <div>
+      <div>
+        <h1 className='uppercase text-4xl font-bold mt-20 text-center'>About me</h1>
+
+        <span className=' border border-b-4 rounded-md border-violet-800 flex justify-center w-[32px] mt-10 ml-[50%]'></span>
+
+        <div className=' flex justify-center'>
+        <p className=' text-center mt-10 text-xl w-[65%] '>Here you will find more information about me, what I do, and my current skills mostly in terms of programming and technology</p>
+
+        </div>
+
+       
+      </div>
+
+      <div className=' flex mx-10 mt-28 gap-20 '>
+        <div className='  w-1/2'>
+          <h2 className=' text-2xl font-bold'>Get to know me!</h2>
+
+        </div>
+
+        <div className='  w-1/2 '>
+          <h2 className=' text-2xl font-bold'>My Skills</h2>
+
+          
+
+          <div className=' mt-10  flex flex-wrap gap-5'>
+          {skills.map((skill, index) => (
+           
+            <button className=' bg-stone-200 py-3 px-5 pointer-events-none rounded-md text-gray-600 font-bold text-sm' >{skill}</button>
+          ))}
+          </div>
+         
+
+        </div>
+      </div>
+    </div>
   )
 }
 
